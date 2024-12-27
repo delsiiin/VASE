d7133f9052a6f334c0decc2f5f56469001aff11d
“delsiiin” <1749970370@qq.com>
2024-12-27 06:41:36 +0000

    update

--- train/train_vicuna.sh
+++ train/train_vicuna.sh
@@ -1,15 +1,15 @@
-CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=2 main.py --model_name_or_path /root/MODELS/vicuna-7b-v1.3 \
+WANDB_MODE="offline" CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=2 main.py --model_name_or_path /root/MODELS/vicuna-7b-v1.3 \
     --data_path /root/idea/speculative_decoding/sharegpt_medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json \
     --bf16 True \
     --output_dir ssd_models/ \
-    --num_train_epochs 1 \
+    --num_train_epochs 3 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "steps" \
-    --eval_steps 1000 \
+    --eval_steps 2000 \
     --save_strategy "steps" \
-    --save_steps 1000 \
+    --save_steps 2000 \
     --learning_rate 2e-3 \
     --weight_decay 0.0 \
     --warmup_ratio 0.1 \
@@ -19,10 +19,11 @@ CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=2 main.py --model_name_or_pat
     --model_max_length 2048 \
     --lazy_preprocess False \
     --top_layers_len 24 \
-    --top_k_group 5 \
+    --top_k_group 3 \
     --resnet_num 1 \
-    --attn_hid_dim 512  \
-    --ssd_decay_coefficient 0.8 
+    --attn_hid_dim 1024  \
+    --ssd_decay_coefficient 0.8 \
+    --model_type vicuna 
 
 # WANDB_MODE="offline" CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=2 main.py --model_name_or_path /root/MODELS/vicuna-7b-v1.3 \
 #     --data_path /root/idea/speculative_decoding/sharegpt_medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json \
