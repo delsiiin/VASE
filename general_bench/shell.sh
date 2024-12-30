a13675e7db8644a2e68934db7e7ed60d72f2db90
“delsiiin” <1749970370@qq.com>
2024-12-30 13:14:21 +0000

    update

--- general_bench/shell.sh
+++ general_bench/shell.sh
@@ -8,6 +8,16 @@ python answer_vicuna.py --model_name /root/MODELS/vicuna-7b-v1.3 --model_id vicu
 python speed.py --model_name /root/MODELS/vicuna-7b-v1.3 --model_id vicuna-7b-v1.3 --bench_name mt_bench --attn
 
 
+# vicuna 13b
+python answer_vicuna.py --model_name /root/MODELS/vicuna-13b-v1.3 --model_id vicuna-13b-v1.3 --bench_name mt_bench --ssd_name /root/idea/speculative_decoding/VASE/train/ssd_models/vicuna-13b-v1.3_ssd_3_lr_0.002_dim_1280 --attn
+
+python answer_vicuna.py --model_name /root/MODELS/vicuna-13b-v1.3 --model_id vicuna-13b-v1.3 --bench_name mt_bench --medusa
+
+python answer_vicuna.py --model_name /root/MODELS/vicuna-13b-v1.3 --model_id vicuna-13b-v1.3 --bench_name mt_bench
+
+python speed.py --model_name /root/MODELS/vicuna-13b-v1.3 --model_id vicuna-13b-v1.3 --bench_name mt_bench --attn
+
+
 # llama2 7b
 python answer_llama2.py --model_name /root/MODELS/Llama-2-7b-chat-hf --model_id llama-2-7b-chat --bench_name mt_bench --max_seq_length 4096 --ssd_name /root/idea/speculative_decoding/VASE/train/ssd_models/Llama-2-7b-chat-hf_ssd_3_lr_0.002_dim_1024 --attn
 
