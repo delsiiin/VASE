22c4834a4c36fcf813f0e47e0b6233f306a9d7ca
“delsiiin” <1749970370@qq.com>
2025-01-02 07:30:15 +0000

    update

--- general_bench/shell.sh
+++ general_bench/shell.sh
@@ -34,3 +34,11 @@ python answer_llama2.py --model_name /root/MODELS/Llama-2-13b-chat-hf --model_id
 python answer_llama2.py --model_name /root/MODELS/Llama-2-13b-chat-hf --model_id llama-2-13b-chat --bench_name mt_bench --max_seq_length 4096 --load_in_8bit
 
 python speed.py --model_name /root/MODELS/Llama-2-13b-chat-hf --model_id llama-2-13b-chat --bench_name mt_bench --attn
+
+
+# llama3 8b
+python answer_llama3.py --model_name /root/MODELS/Meta-Llama-3-8B-Instruct --model_id llama-3-8b-inst --bench_name mt_bench --max_seq_length 4096 --ssd_name /root/idea/speculative_decoding/VASE/train/ssd_models/Meta-Llama-3-8B-Instruct_ssd_3_lr_0.002_dim_1024 --attn
+
+python answer_llama3.py --model_name /root/MODELS/Meta-Llama-3-8B-Instruct --model_id llama-3-8b-inst --bench_name mt_bench --max_seq_length 4096
+
+python speed.py --model_name /root/MODELS/Meta-Llama-3-8B-Instruct --model_id llama-3-8b-inst --bench_name mt_bench --attn
