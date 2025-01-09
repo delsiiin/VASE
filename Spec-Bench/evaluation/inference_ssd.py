e9c75b08bc46d6467adbf1f8e8268120f07b7551
“delsiiin” <1749970370@qq.com>
2025-01-09 06:09:27 +0000

    update

--- Spec-Bench/evaluation/inference_ssd.py
+++ Spec-Bench/evaluation/inference_ssd.py
@@ -10,6 +10,7 @@ from evaluation.eval import reorg_answer_file
 from fastchat.utils import str_to_torch_dtype
 
 from model.ssd.utils import *
+from model.ssd.utils_eagle import *
 from model.ssd.ssd_model import SSDModel
 from model.ssd.kv_cache import initialize_past_key_values, initialize_past_key_values_attn, initialize_past_key_values_llama, initialize_past_key_values_llama_attn
 from model.ssd.medusa_choices import *
@@ -115,6 +116,103 @@ def ssd_forward(inputs, model, tokenizer, max_new_tokens, medusa_choices=None, t
         if new_token > max_new_tokens:
             break
     return input_ids, new_token, idx+1, accept_length_list
+
+def ssd_forward_eagle(inputs, model, tokenizer, max_new_tokens, medusa_choices=None, temperature=0.0, posterior_threshold=0.09, posterior_alpha=0.3, max_steps=512):
+    max_length=2048-model.router.total_tokens-10
+    input_ids = inputs.input_ids
+    assert input_ids.shape[0] == 1, "Only support batch size 1 for now!!"
+    # Avoid modifying the input_ids in-place
+    input_ids = input_ids.clone()
+    accept_length_list = []
+
+    # Initialize the past key and value states
+    if hasattr(model, "past_key_values"):
+        past_key_values = model.past_key_values
+        past_key_values_data = model.past_key_values_data
+        current_length_data = model.current_length_data
+        # Reset the past key and value states
+        current_length_data.zero_()
+
+        past_key_values_attn = model.past_key_values_attn
+        past_key_values_data_attn = model.past_key_values_data_attn
+        current_length_data_attn = model.current_length_data_attn
+        # Reset the past key and value states
+        current_length_data_attn.zero_()
+    else:
+        (
+            past_key_values,
+            past_key_values_data,
+            current_length_data,
+        ) = initialize_past_key_values(model)
+        model.past_key_values = past_key_values
+        model.past_key_values_data = past_key_values_data
+        model.current_length_data = current_length_data
+
+        past_key_values_attn, past_key_values_data_attn, current_length_data_attn = initialize_past_key_values_attn(model)
+
+        model.past_key_values_attn = past_key_values_attn
+        model.past_key_values_data_attn = past_key_values_data_attn
+        model.current_length_data_attn = current_length_data_attn
+
+    padding=torch.zeros((1,1), dtype=torch.long, device=input_ids.device)
+
+    input_len = input_ids.shape[1]
+    cur_length = input_len
+    reset_ssd_mode_ea(model, True)
+    new_token = 0
+
+    if temperature > 1e-5:
+        logits_processor = prepare_logits_processor(temperature=temperature, top_p=0, top_k=0)
+    else:
+        logits_processor = None
+    
+    draft_tokens, retrieve_indices,tree_mask,tree_position_ids, logits, hidden_states = initialize_ssd_ea_hid(
+        input_ids, model, logits_processor
+    )
+
+    for idx in range(max_steps): # idx: new decoding steps
+
+        model.base_model.model.ssd_mask = tree_mask
+        model.router.ssd_mask = tree_mask
+
+        draft_tokens=draft_tokens.to(input_ids.device)
+        
+        logits, hidden_states_new = ssd_tree_decoding_ea_hid(
+                model,
+                draft_tokens,
+                tree_position_ids,
+                input_ids,
+                retrieve_indices,
+            )
+        draft_tokens=torch.cat((draft_tokens, padding),dim=1)
+        candidates=draft_tokens[0,retrieve_indices]
+
+        best_candidate, accept_length, sample_p = evaluate_posterior_ea(
+                logits, candidates, logits_processor=logits_processor
+            )
+        input_ids, draft_tokens, retrieve_indices,tree_mask,tree_position_ids, new_token = update_inference_inputs_ssd_ea_hid(
+                input_ids,
+                candidates,
+                best_candidate,
+                accept_length,
+                retrieve_indices,
+                new_token,
+                model,
+                hidden_states_new,
+                sample_p,
+                logits_processor
+            )
+        accept_length_tree = input_ids.shape[1] - cur_length
+        cur_length = accept_length_tree + cur_length
+        accept_length_list.append(accept_length_tree)
+        if tokenizer.eos_token_id in input_ids[0, input_len:].tolist():
+            break
+        if new_token > max_new_tokens:
+            break
+        if input_ids.shape[1] > max_length:
+            break
+    return input_ids, new_token, idx+1, accept_length_list
+
 
 
 def ssd_forward_llama(inputs, model, tokenizer, max_new_tokens, medusa_choices=None, temperature=0.0, posterior_threshold=0.09, posterior_alpha=0.3, max_steps=512, model_id=None):
