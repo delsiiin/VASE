f16b385fc9851503c904372c5867bbe82ed204a4
“delsiiin” <1749970370@qq.com>
2025-01-11 13:18:32 +0000

    update

--- gen_data/shell.sh
+++ gen_data/shell.sh
@@ -1,23 +1,29 @@
 # vicuna 7b
-python allocation.py --outdir train_data_vicuna7b \
-                    --type vicuna \
-                    --basemodel /root/MODELS/vicuna-7b-v1.3 \
-                    --data /root/idea/speculative_decoding/sharegpt_medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json 
+# python allocation.py --outdir train_data_vicuna7b \
+#                     --type vicuna \
+#                     --basemodel /root/MODELS/vicuna-7b-v1.3 \
+#                     --data /root/idea/speculative_decoding/sharegpt_medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json 
 
 # vicuna 13b 8bit
-python allocation.py --outdir train_data_vicuna13b \
-                    --type vicuna \
-                    --basemodel /root/MODELS/vicuna-13b-v1.3 \
-                    --data /root/idea/speculative_decoding/sharegpt_medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json 
+# python allocation.py --outdir train_data_vicuna13b \
+#                     --type vicuna \
+#                     --basemodel /root/MODELS/vicuna-13b-v1.3 \
+#                     --data /root/idea/speculative_decoding/sharegpt_medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json 
 
 # llama2 7b
-python allocation.py --outdir train_data_llama27b \
-                    --type vicuna \
-                    --basemodel /root/MODELS/Llama-2-7b-chat-hf \
-                    --data /root/idea/speculative_decoding/sharegpt_medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json 
+# python allocation.py --outdir train_data_llama27b \
+#                     --type llama2 \
+#                     --basemodel /root/MODELS/Llama-2-7b-chat-hf \
+#                     --data /root/idea/speculative_decoding/sharegpt_medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json 
 
 # llama2 13b 8bit
-python allocation.py --outdir train_data_llama213b \
-                    --type vicuna \
-                    --basemodel /root/MODELS/Llama-2-13b-chat-hf \
+# python allocation.py --outdir train_data_llama213b \
+#                     --type llama2 \
+#                     --basemodel /root/MODELS/Llama-2-13b-chat-hf \
+#                     --data /root/idea/speculative_decoding/sharegpt_medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json 
+
+# llama3 8b
+python allocation.py --outdir train_data_llama38b \
+                    --type llama3 \
+                    --basemodel  /root/MODELS/Meta-Llama-3-8B-Instruct \
                     --data /root/idea/speculative_decoding/sharegpt_medusa/ShareGPT_V4.3_unfiltered_cleaned_split.json 
